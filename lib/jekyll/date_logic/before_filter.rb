bddc701dd3a00eb1655247b217ab1fc693fd71ce
Greg Scott <i@gregoryjscott.com>
2015-07-16 12:25:28 -0500

    Fix array filters to support Jekyll collections

--- lib/jekyll/date_logic/before_filter.rb
+++ lib/jekyll/date_logic/before_filter.rb
@@ -7,10 +7,23 @@ module Jekyll
       def before(input, date, for_hours = nil)
         return [] if input.nil?
         future_dates = input.select do |item|
-          next true if item[date].nil?
-          time = Time.parse(item[date].to_s)
+          item = item_property(item, date)
+          next true if item.nil?
+          time = Time.parse(item.to_s)
           for_hours_number = ForHours.parse(for_hours) unless for_hours.nil?
           Before.qualifies?(time, for_hours_number)
+        end
+      end
+
+      private
+
+      def item_property(item, property)
+        if item.respond_to?(:to_liquid)
+          item.to_liquid[property.to_s]
+        elsif item.respond_to?(:data)
+          item.data[property.to_s]
+        else
+          item[property.to_s]
         end
       end
 
